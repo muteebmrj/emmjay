3739a60da1efbf4ede267a1ab88b47706cbb5f07
muteeb <muteebmrj@gmail.com>
2021-12-22 16:10:49 +0530

    jj

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,17 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($(".pre-loader").length > 0)
+        {
+            $(".pre-loader").fadeOut("slow");
+        }
+    });
+})(jQuery)
+
+
 (function() {
   "use strict";
 
